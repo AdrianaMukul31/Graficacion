caf063780f0e524591e3c0859e3dd094ae32f090
Adriana <isabelbriceno3110@gmail.com>
2025-09-18 10:53:31 -0600

    feat(rive): wire up isChecking/isHandsUp inputs on login

--- login_with_animation/lib/screens/login_screen.dart
+++ login_with_animation/lib/screens/login_screen.dart
@@ -11,8 +11,17 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   bool _obscurePassword = true; // Debe estar en el State
 
+  //cerebro de la logica de aanimacciones SMI
+  StateMachineController? controller; //los signos verifican que todo este bien
+  SMIBool? isChecking; //activa la animacion de checar o ver lo que escribes
+  SMIBool? isHandsUp;  //se tapa los ojos
+  SMIBool? trigSuccess; //se emociona o se pone feliz
+  SMIBool? trigFail;    //animacion de fracaso o se pone triste
+
+
   @override
   Widget build(BuildContext context) {
+    //consulta el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -24,12 +33,40 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: const RiveAnimation.asset(
+                child: RiveAnimation.asset(
                   'assets/animated_login_character.riv',
+                  stateMachines: ["Login Machine"],
+                  //Al iniciarse
+                  onInit: (artboard){
+                    controller = 
+                    StateMachineController.fromArtboard(
+                      artboard,
+                      "Login Machine",
+                      );
+                      //verificar que inicio bien
+                      if(controller == null) return;
+                      artboard.addController(controller!);
+                      //asignar las variables y enlazarlas
+                      isChecking  = controller!.findSMI('isChecking');
+                      isHandsUp   = controller!.findSMI('isHandsUp');
+                      trigSuccess = controller!.findSMI('trigSuccess');
+                      trigFail    = controller!.findSMI('trigFail');
+                  },
                 ),
               ),
               const SizedBox(height: 20),
+              //Email
               TextField(
+                onChanged: (value) {
+                  //si el valor es mayor a 0 activa la animacion de checar
+                  if(isHandsUp != null){
+                    //No tapar los ojos al escribir email
+                    isHandsUp!.change(false);
+                  }
+                  if(isChecking == null) return;
+                  //Activa el modo chismoso
+                  isChecking!.change(true);
+                },
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                   hintText: "Email",
@@ -40,7 +77,18 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
               const SizedBox(height: 10),
+              //Password
               TextField(
+                onChanged: (value) {
+                  //si el valor es mayor a 0 activa la animacion de checar
+                  if(isHandsUp != null){
+                    //No tapar los ojos al escribir email
+                    isHandsUp!.change(true);
+                  }
+                  if(isChecking == null) return;
+                  //Activa el modo chismoso
+                  isChecking!.change(false);
+                },
                 obscureText: _obscurePassword,
                 keyboardType: TextInputType.visiblePassword,
                 decoration: InputDecoration(
@@ -56,11 +104,59 @@ class _LoginScreenState extends State<LoginScreen> {
                     onPressed: () {
                       setState(() {
                         _obscurePassword = !_obscurePassword;
+                        isHandsUp!.change(false);
                       });
                     },
                   ),
                 ),
               ),
+              const SizedBox(height: 10),
+              //Texto "Forgot Password?" alineado a la derecha
+              SizedBox(
+                width:size.width,
+                child: const Text("Forgot Password?", 
+                //Aliniar a la derecha
+                textAlign: TextAlign.right,
+                style: TextStyle(decoration: TextDecoration.underline),
+                ),
+                ),
+                //Boton de login
+              const SizedBox(height: 10),
+              MaterialButton(
+              minWidth:size.width,
+              height: 50,
+              color: Colors.purple,
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+                onPressed: () {},
+                child: Text("Login", 
+                style: TextStyle(
+                  color: Colors.white)),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [                  
+                  const Text("Don't have an account?"),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("Sign Up",
+                     style: TextStyle(
+                      color: Colors.black,
+                      //en negritas
+                      fontWeight: FontWeight.bold,
+                      //subrayado
+                      decoration: TextDecoration.underline,
+                      ),
+                      ),
+                  ),
+                ],
+
+                ),
+              )
             ],
           ),
         ),
